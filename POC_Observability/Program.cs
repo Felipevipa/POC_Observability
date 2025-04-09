780bd6c213199f5a69bd1bb6a3755a49c3cafcb2
AzureAD\AndresFelipeVillamiz <avillamizar@premier.pr>
2025-04-09 15:26:22 -0500

    first commit

---
+++ POC_Observability/Program.cs
@@ -0,0 +1,37 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day)
+    .Enrich.FromLogContext()
+    .MinimumLevel.Debug()
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Use the Serilog extension method for IHostBuilder
+builder.Host.UseSerilog();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
